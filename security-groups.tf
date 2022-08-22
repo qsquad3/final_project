d52b5c7e3704bc1a2c728e730d5fa6fe94c17d45
qsquad3 <111643131+qsquad3@users.noreply.github.com>
2022-08-22 20:03:13 -0300

    Create security-groups.tf

---
+++ security-groups.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "default-sg" {
+   name        = "default-sg"
+   description = "Default Security Group"
+   #vpc_id      = aws_vpc.prod-vpc.id
+
+   ingress {
+     description = "HTTPS"
+     from_port   = 443
+     to_port     = 443
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+   ingress {
+     description = "HTTP"
+     from_port   = 80
+     to_port     = 80
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+   ingress {
+     description = "SSH"
+     from_port   = 22
+     to_port     = 22
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   egress {
+     from_port   = 0
+     to_port     = 0
+     protocol    = "-1"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   tags = {
+     Name = "Default SG"
+   }
+ }
