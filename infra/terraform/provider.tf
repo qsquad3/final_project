743001a2af41dcb82597098477dfb3a377beb362
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2022-09-09 01:33:37 -0300

    add export deamon docker

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-   access_key = var.accesskey
-   secret_key = var.secretkey
-   #profile = "tf12"
-   #ssh_keypair = var.ssh_keypair
-  region     = "us-east-1"
+  access_key = var.accesskey
+  secret_key = var.secretkey
+  #profile = "tf12"
+  #ssh_keypair = var.ssh_keypair
+  region = "us-east-1"
 }
 
 terraform {
   backend "s3" {
-    bucket = "terraform-state-project"
+    bucket = "terraform-state-project-quode"
     key    = "tf.tfstate"
     region = "us-east-1"
   }
